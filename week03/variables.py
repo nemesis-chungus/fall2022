24b7d347312b45c085b7bffa1ba729710311f4a2
nemesis.chungus <nemesis.chungus@gmail.com>
2022-10-04 10:41:30 -0400

    python week03 assignment

--- week03/variables.py
+++ week03/variables.py
@@ -1,3 +1,17 @@
 x = 10
 y = 20
-print(f"{x} plus {y} is (x + y)")
+print(x, y)
+print("x is", x, "and y is", y)
+print(x, "multiplied by", y, "is",x * y)
+print(x, "multiplied by", y, "is",x * y)
+x = 13.5
+print(x, "divided by", y, "is", x / y)
+x = 10
+y = 20
+result = x + y
+print(x, "plus", y, "is", result)
+a1 = "cat"
+a2 = "dog"
+print("pets:", a1, a2)
+print("%dx%d=%d" % (x, y, x * y))
+print(f"{x} plus {y} is {x + y}")
