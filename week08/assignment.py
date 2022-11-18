b761f66cff73b84c683ca5f887b7f9aa55a9d5f2
nemesis-chungus <113367369+nemesis-chungus@users.noreply.github.com>
2022-11-18 08:56:29 -0500

    Python week08 assignment

---
+++ week08/assignment.py
@@ -0,0 +1,14 @@
+import os
+
+sourceDir = os.path.dirname(__file__)
+
+f = open(os.path.join(sourceDir, "week8-output.txt"))
+lines = f.readlines()
+words = (" ")
+words = lines
+for word in words:
+    print({word.strip()})
+    count = len(words)
+if word.strip() != " ":
+    print(f"words: {len(words)}")
+f.close
