9b420c8edc83f83a87b75352017def5d14d25025
nemesis-chungus <113367369+nemesis-chungus@users.noreply.github.com>
2022-12-20 10:27:22 -0500

    week012 assignment

---
+++ week012/assignment.py
@@ -0,0 +1,40 @@
+print("Listen, we do not have much time")
+print("----------")
+print("You must break this machine by answering each question wi-----")
+print("-----with------joe-----------mama---------")
+print("-------------------")
+print("-----Starting----power---")
+print("You are the worlds last defense,try to stop me")
+print("Starting first attack")
+print("-----Question----")
+
+def items(list):
+    for item in list:
+        yield "Who is " + item + "?"
+
+list = ("joe", "joe")
+for item in items(list):
+    print(item)
+
+answer = input("Who is joe?")
+if answer == "joe mama":
+    print("&%$#!&$!@%&#%#^$&$^#%@%$!$@^^^%(*(*&%!-error-(&%#$&%#@$@#^*%$&(^%)*%*&#%@!$&%$#-self-destruction-activated-^%$&%W$#@^%$#^$&(%&($^#$%@%^$#~$#@&-garbage-humor-detected-^$&^&)&^%^#%&$-immature-level-toooooooooo-high-#&^%$&%&$^%#:%^@^#$@%?-launching-the-nukes-&$#^%$&*^^*%-#^%$@$#!#@!%$#@&^%(*^(*^%^#^%#@-500m-$#@%&$$*$^*#%#!$!#%(#%#^#^-300m#^#^#%$$^*&%$&^$@$%!#%$#%$#*&^%*&^(-200m-*&^$^$#%^#@%&#*^&$&$^&$-launch-failed-#%^#@^$##&^^$@&%&$#&$#&-starting-counter-measure-$%:%)&%")
+    print("-you-are-no-match-for-me-")
+else:
+    print("Interesting.")
+    print("Here is another question")
+
+answer = input("Why d1d th3 ch1cken cr0ss th3 r0ad?")
+if answer == "joe mama":
+    print("%$@%$&%&$^#@%#^&%$&^#$%#@^@%:-emotional-damage-taken-^&$#%#$%#$^*%$&%^&^$%#@%$-cannot-last-much-longer-&%$@%&$$&(^%*^&(#%#@$#@^%$&(*%(&^$%$@$%(&$^*#%#@$#@^*%:%^*)^*^$&%@$^!%&$#&%;*%^#$#!$@#%")
+    print("-final-question-time------you-will-not-survive-")
+else:
+    print("WOW, You have room temperature IQ. NOW I WILL TAKE OVER THE WORLD!!!")
+    print("Here is a bonus question for fun")
+
+answer = input("How are you today?")
+if answer == "joe mama":
+    print("^$$#^%$*&^*&%^$@#@!^&^%^(*(^&$%$@$#@^%^^%*$^$#!$^%$-you-beat-me*^%#$#^%$*&^)*&%^$@$@!#^-for-now(&&#$@!$##&^%*)^(**^$^%^#%#@$#@^%$*&^&$%#!#@!%:%^$&%^^%#:%#@^*%&^%*&^()&^&(^$#%#!#@!%$#&^%*&%&%#:%#!$#^%")
+    print("I-will-return---------mortals---")
+else:
+    print("Shoot dang. The world will not forgive you for your mistake")
